3cafad2c1c6998117598cc84ea7e9a7c1b29889d
Steven Cooper <stevengcooper76@gmail.com>
2016-03-04 12:38:01 -0500

    Generated controllers have private methods and update and create methods.

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -5,7 +5,47 @@ class ExercisesController < ApplicationController
   def post
   end
 
+  def create
+    @exercise = Exercise.new(exercise_params)
+    respond_to do |format|
+      if @exercise.save
+        format.html { redirect_to @exercise, notice: 'Exercise was successfully created.' }
+        format.json { render :show, status: :created, location: @exercise }
+      else
+        format.html { render :new }
+        format.json { render json: @exercise.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /exercises/1
+  # PATCH/PUT /exercises/1.json
+  def update
+    respond_to do |format|
+      if @exercise.update(exercise_params)
+        format.html { redirect_to @exercise, notice: 'Exercise was successfully updated.' }
+        format.json { render :show, status: :ok, location: @exercise }
+      else
+        format.html { render :edit }
+        format.json { render json: @exercise.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @exercise.destroy
+    respond_to do |format|
+      format.html { redirect_to exercises_url, notice: 'Exercise was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+
   private
     def choloric_intake
       params.require(:exercise).permit(:calories_burned_per_minute, :exercise_type_id, :duration)
+    end
+
+    def set_exercise
+      @exercise = Exercise.find(params[:id])
+    end
+
 end
