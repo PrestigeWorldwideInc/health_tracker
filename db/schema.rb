c2ce48f5d9f3502ff08e578cf13752591b4cdd15
Steven Cooper <stevengcooper76@gmail.com>
2016-03-05 12:36:55 -0500

    Schema page requests a commit.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160305164757) do
-=======
-ActiveRecord::Schema.define(version: 20160304234937) do
->>>>>>> 82e94d633debc19ccaebd9fc989ec6b5497f3c47
 
   create_table "choloric_intakes", force: :cascade do |t|
     t.decimal  "calories"
@@ -36,13 +32,6 @@ ActiveRecord::Schema.define(version: 20160304234937) do
     t.integer  "exercise_type_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-  end
-
-  create_table "step_counters", force: :cascade do |t|
-    t.integer  "steps_taken"
-    t.date     "date"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "step_counts", force: :cascade do |t|
