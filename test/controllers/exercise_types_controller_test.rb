9e7a524b3589ddd06deff8563d55546396d9478f
Steven Cooper <stevengcooper76@gmail.com>
2016-03-05 13:19:01 -0500

    Choloric Intake controller tests all pass.

--- test/controllers/exercise_types_controller_test.rb
+++ test/controllers/exercise_types_controller_test.rb
@@ -1,14 +1,25 @@
 require 'test_helper'
 
 class ExerciseTypesControllerTest < ActionController::TestCase
+  setup do
+    @exercise_type = exercise_type(:one)
+  end
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:exercise_type)
   end
 
-  test "should get post" do
-    get :post
+  test "should get new" do
+    get :new
     assert_response :success
   end
 
+  test "should create exercise_type" do
+    assert_difference('ExerciseType.count') do
+      post :create, exercise_type: { type: @exercise_type.type }
+    end
+
+    assert_redirected_to exercise_type_path(assigns(:exercise_type))
+  end
 end
