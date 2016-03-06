04693d99bc245d36c6a7309d877f00f4d789d5f2
Daniel Buompane <danielbuompane@gmail.com>
2016-03-05 20:52:18 -0500

    Complete refactoring to pass exercises_controller tests

--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -23,26 +23,18 @@ class ExercisesControllerTest < ActionController::TestCase
 
     assert_redirected_to exercise_path(assigns(:exercise))
   end
-  #
-  # test "should show exercise" do
-  #   get :show, id: @exercise
-  # end
-  #
-  # test "should get edit" do
-  #   get :edit, id: @exercise
-  #   assert_response :success
-  # end
-  #
-  # test "should update exercise" do
-  #   patch :update, id: @exercise, exercise: {calories_burned_per_minute: @exercise.calories_burned_per_minute, duration: @exercise.duration, exercise_type: @exercise.exercise_type_id}
-  #   assert_redirected_to exercise_path(assigns(:exercise))
-  # end
-  #
-  # test "should destroy exercise" do
-  #   assert_difference('Exercise.count', -1) do
-  #     delete :destroy, id: @exercise
-  #   end
-  #
-  #   assert_redirected_to exercises_path
-  # end
+
+  test "should update exercise" do
+    patch :update, id: @exercise, exercise: {calories_burned_per_minute: @exercise.calories_burned_per_minute, duration: @exercise.duration}
+
+    assert_redirected_to exercise_path(assigns(:exercise))
+  end
+
+  test "should destroy exercise" do
+    assert_difference('Exercise.count', -1) do
+      delete :destroy, id: @exercise
+    end
+
+    assert_redirected_to exercises_path
+  end
 end
